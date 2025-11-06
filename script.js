71ae56cc097bc49634038824a80515acb5d3823a
JuanPavi25 <juancamilopavi9@gmail.com>
2025-11-06 00:10:23 -0500

    Detalles finales

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
-  const toggleBtn = document.getElementById('toggleMenu');
-  const sidebar = document.getElementById('sidebar');
+const toggleBtn = document.getElementById('toggleMenu');
+const sidebar = document.getElementById('sidebar');
+const fondo = document.getElementById('fondo');
 
-  toggleBtn.addEventListener('click', () => {
+toggleBtn.addEventListener('click', () => {
     sidebar.classList.toggle('active');
-  });
+    fondo.classList.toggle('active');
+});
+
+fondo.addEventListener('click', () => {
+    sidebar.classList.remove('active');
+    fondo.classList.remove('active');
+});
+
